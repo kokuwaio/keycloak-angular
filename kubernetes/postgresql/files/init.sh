0fb8eaa3642866780b917bbaabca6c9b79078158
Stephan Schnabel <stephan.schnabel@posteo.de>
2023-03-31 12:03:58 +0200

    fix db

--- kubernetes/postgresql/files/init.sh
+++ kubernetes/postgresql/files/init.sh
@@ -7,8 +7,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER "keycloak" WITH ENCRYPTED PASSWORD 'changeMe';
     GRANT ALL PRIVILEGES ON DATABASE "keycloak" TO "keycloak";
 
-    CREATE DATABASE "grafana";
-    CREATE USER "grafana" WITH ENCRYPTED PASSWORD 'changeMe';
-    GRANT ALL PRIVILEGES ON DATABASE "grafana" TO "grafana";
-
 EOSQL
